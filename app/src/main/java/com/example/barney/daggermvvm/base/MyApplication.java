128a849b441d61d3964e2708f574873bf4c27fed
3barney <brnbsjm@gmail.com>
2018-10-04 12:45:56 +0300

    build component object and inject the application module into it

--- app/src/main/java/com/example/barney/daggermvvm/base/MyApplication.java
+++ app/src/main/java/com/example/barney/daggermvvm/base/MyApplication.java
@@ -4,8 +4,15 @@ import android.app.Application;
 
 public class MyApplication extends Application {
 
+    private ApplicationComponent mApplicationComponent;
+
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // Build component Object. and turn it to a dagger component
+        mApplicationComponent = DaggerApplicationComponent.builder()
+                .applicationModule(new ApplicationModule(this))
+                .build();
     }
 }
